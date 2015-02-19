1d64d99e5cc1807ba9470b4381f18a45412fb8ac
steve <steve.bignell@gmail.com>
2015-02-19 16:24:35 +1100

    added jade to grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -200,6 +200,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-newer');
 
   //grunt.registerTask('default', ['copy:vendor', 'newer:uglify', 'newer:less', 'concurrent']);
-  grunt.registerTask('build', ['copy:vendor', 'uglify', 'less']);
+  grunt.registerTask('build', ['copy:vendor', 'jade', 'uglify', 'less']);
   grunt.registerTask('lint', ['jshint']);
 };
